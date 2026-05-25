04fd354ca44a6b42a117f9c560ef9304f221c19a
fazaaaiyaitu-hash <fazaaaiyaitu@gmail.com>
2026-05-26 02:05:06 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from
 "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+    apiKey: "AIzaSyBvjdO78HslTn9RHM4-tLsdQbJyKsSsxJk",
+    authDomain: "website-5fe39.firebaseapp.com",
+    projectId: "website-5fe39",
+    storageBucket: "website-5fe39.firebasestorage.app",
+    messagingSenderId: "243807840965",
+    appId: "G-C2V2JBT1BT"
 };
 
 const app = initializeApp(firebaseConfig);
